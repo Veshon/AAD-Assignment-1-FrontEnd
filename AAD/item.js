a2fee46e629177e8b55f59b653bc64ba48e8982f
veshon <vashonmabima12@gmail.com>
2024-08-20 11:00:01 +0530

    Item UPDATE

--- AAD/item.js
+++ AAD/item.js
@@ -67,4 +67,44 @@ $(document).ready(function () {
             }
         });
     });
+
+    /////////////////////////////////////Update///////////////////////////////////////////
+    $("#update").click(function() {
+        event.preventDefault();
+
+        let codeE = $("#code").val();
+        let descE = $("#desc").val();
+        let qtyE = $("#qty").val();
+        let priceE = $("#price").val();
+
+        console.log(codeE);
+        console.log(descE);
+        console.log(qtyE);
+        console.log(priceE);
+
+        const itemData = {
+            code: codeE,
+            description: descE,
+            qty: qtyE,
+            price: priceE
+        };
+
+        console.log(itemData)
+
+        const itemJSON = JSON.stringify(itemData)
+        console.log(itemJSON)
+
+        $.ajax({
+            url: "http://localhost:8080/item?code="+codeE,
+            type: "PUT",
+            data: itemJSON,
+            headers: { "Content-Type": "application/json" },
+            success: (res) => {
+                console.log(JSON.stringify(res));
+            },
+            error: (res) => {
+                console.error(res);
+            }
+        })
+    })
 })
